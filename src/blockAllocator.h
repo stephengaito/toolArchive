6cc90ceff7e9f37530d4fce716b4a89e4a6a1cca
Stephen Gaito <stephen@perceptisys.co.uk>
2015-05-26 13:17:02 +0100

    added first pass at a centralized (inline) block allocator

---
+++ src/blockAllocator.h
@@ -0,0 +1,107 @@
+#ifndef BLOCK_ALLOCATOR_H
+#define BLOCK_ALLOCATOR_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#ifndef BlockIncrement
+#define BlockIncrement 10
+#endif
+
+/// \brief The BlockAllocator class holds the information required
+/// to allocate multiple blocks of related (sub)structures.
+class BlockAllocator {
+  public:
+
+    /// \brief Create a new block allocator which allocates a given
+    /// blockSize.
+    BlockAllocator(size_t aBlockSize) {
+      numBlocks = BlockIncrement;
+      nextBlock = 0;
+      blockSize = aBlockSize;
+      blocks    = (char**)calloc(numBlocks, sizeof(char*));
+      curAllocationByte = NULL;
+      endAllocationByte = NULL;
+      for (size_t i = 0; i < numBlocks; i++) {
+        blocks[i] = NULL;
+      }
+    }
+
+    /// \brief Clear (free) all of the blocks.
+    void clearBlocks(void) {
+      for (size_t i = 0; i < numBlocks; i++) {
+        if (blocks[i]) {
+          free(blocks[i]);
+          blocks[i] = NULL;
+        }
+      }
+    }
+
+    /// \brief Destory the block allocator and all of its blocks.
+    ~BlockAllocator(void) {
+      clearBlocks();
+      numBlocks = 0;
+      nextBlock = 0;
+      blockSize = 0;
+      curAllocationByte = NULL;
+      endAllocationByte = NULL;
+    }
+
+  private:
+    // \brief Add a new allocation block to this blockAllocator.
+    void addNewBlock(void) {
+      if (numBlocks <= nextBlock) {
+        // we do not have enough block pointers so increase the
+        // size of the blocks array.
+        char **oldBlocks = blocks;
+        blocks =
+          (char**)calloc(numBlocks + BlockIncrement, sizeof(char*));
+        if (oldBlocks) {
+          memcpy(blocks, oldBlocks, numBlocks*sizeof(char*));
+          free(oldBlocks);
+        }
+        numBlocks += BlockIncrement;
+      }
+      blocks[nextBlock] = (char*)calloc(blockSize, 1);
+      curAllocationByte = blocks[nextBlock];
+      endAllocationByte = curAllocationByte + blockSize + 1;
+      nextBlock++;
+    }
+
+  public:
+    /// \brief Allocate a new (sub)structure of the given size.
+    char *allocateNewStructure(size_t structureSize) {
+      if (endAllocationByte <= curAllocationByte + structureSize) {
+        // we need to allocate a new block
+        addNewBlock();
+      }
+      char *newStructure = curAllocationByte;
+      curAllocationByte += structureSize;
+      return newStructure;
+    }
+
+  private:
+
+    /// \brief The current block from which allocations are being made.
+    char *curAllocationByte;
+
+    /// \brief The end of the current allocation block.
+    char *endAllocationByte;
+
+    /// \brief The index of next (but currently unallocated) allocation
+    /// block in the block array.
+    size_t nextBlock;
+
+    /// \brief The total number of possible allocation blocks in the
+    /// block array.
+    size_t numBlocks;
+
+    /// \brief The size of each new allocation block
+    size_t blockSize;
+
+    /// \brief The blocks from which to allocate new sub-structures.
+    char **blocks;
+};
+
+#endif
---
+++ tests/blockAllocatorTests.cpp
@@ -0,0 +1,71 @@
+#include <bandit/bandit.h>
+using namespace bandit;
+
+#include <string.h>
+#include <stdio.h>
+#include <exception>
+
+#ifndef private
+#define private public
+#endif
+
+#include <stdio.h>
+#include <blockAllocator.h>
+
+go_bandit([](){
+
+  printf("\n----------------------------------\n");
+  printf(  "blockAllocator\n");
+  printf(  "BlockAllocator = %zu bytes (%zu bits)\n", sizeof(BlockAllocator), sizeof(BlockAllocator)*8);
+  printf(  "----------------------------------\n");
+
+  /// \brief We test the correctness of the C-based BlockAllocator structure.
+  ///
+  describe("BlockAllocator", [](){
+
+    it("CreateBlockAllocator should create a working block allocator", [&](){
+      BlockAllocator *blockAllocator = new BlockAllocator(10);
+      AssertThat(blockAllocator, Is().Not().EqualTo((void*)0));
+      AssertThat(blockAllocator->blockSize, Is().EqualTo(10));
+      for (size_t i = 0; i < blockAllocator->numBlocks; i++) {
+        AssertThat(blockAllocator->blocks[i], Is().EqualTo((void*)0));
+      }
+      blockAllocator->~BlockAllocator();
+    });
+
+
+    it("AddNewBlock should add a new block", [&](){
+      char* prevPtrs[25];
+      BlockAllocator *blockAllocator = new BlockAllocator(10);
+      for (size_t j = 0; j < 25; j++) {
+        blockAllocator->addNewBlock();
+        prevPtrs[j] = blockAllocator->curAllocationByte;
+        AssertThat(blockAllocator->nextBlock, Is().EqualTo(j+1));
+        for (size_t i = 0; i <= j; i++) {
+         AssertThat(blockAllocator->blocks[i], Is().Not().EqualTo((void*)0));
+         AssertThat(blockAllocator->blocks[i], Is().EqualTo(prevPtrs[i]));
+        }
+      }
+    });
+
+    it("AllocateNewStructure should allocate some new structures", [&](){
+      BlockAllocator *blockAllocator = new BlockAllocator(10);
+      for (size_t j = 0; j < 25; j++) {
+        char *aStructure = blockAllocator->allocateNewStructure(2);
+        AssertThat(aStructure, Is().Not().EqualTo((char*)0));
+      }
+      AssertThat(blockAllocator->numBlocks, Is().EqualTo(10));
+      AssertThat(blockAllocator->nextBlock, Is().EqualTo(5));
+      size_t i = 0;
+      for ( ; i < 5; i++) {
+        AssertThat(blockAllocator->blocks[i], Is().Not().EqualTo((char*)0));
+      }
+      for ( ; i < blockAllocator->numBlocks; i++) {
+        AssertThat(blockAllocator->blocks[i], Is().EqualTo((char*)0));
+      }
+    });
+
+  }); // describe BlockAllocator
+
+});
+
