7422ec420689b247a75d65ff1dce7c83c8613014
Stephen Gaito <stephen@perceptisys.co.uk>
2016-08-24 07:28:21 +0100

    continued attempt to build a simple n-d viewer...

--- ndViewer/inst/bin/exampleShinyApp.R
+++ ndViewer/inst/bin/exampleShinyApp.R
@@ -3,39 +3,78 @@
 # This Rscript loads and runs a shiny app to display the a simple 4-d cube
 
 library(shiny)
+library(dynSysToolKit)
+library(plot3D)
 library(ndViewer)
+
+coordLabels <- c("x", "y", "z")
 
 server <- function(input, output) {
   
-  cube <- matrix(c(
-    0.1,0.1,0,0,
-    0.9,0.1,0,0,
-    0.9,0.9,0,0,
-    0.1,0.9,0,0,
-    0.1,0.1,1,0
-  ), nrow = 5, ncol = 4, byrow = TRUE)
-    
-  output$echo <- renderText(
-    paste(input$vp.test1, input$vp.test2, input$va.test1, input$va.test2, sep = " ")
+  results <- reactive({
+    integrateLorenz(
+      x = as.double(input$x),
+      y = as.double(input$y),
+      z = as.double(input$z),
+      sigma = as.double(input$sigma),
+      rho = as.double(input$rho),
+      beta = as.double(input$beta),
+      stepsPerSample = 1,
+      numSamples = 1000)
+  })
+  
+  output$echo <- renderUI(
+    inputPanel(
+      p("x = ", input$x),
+      p("y = ", input$y),
+      p("z = ", input$z),
+      hr(),
+      p("sigma = ", as.double(input$sigma)),
+      p("rho   = ", as.double(input$rho)),
+      p("beta  = ", as.double(input$beta))
+    )
   )
   
+  output$results <- renderTable(results())
+  
   output$distPlot <- renderPlot({
-    .R_graphicsParameters()
-    lines(cube[,1], cube[,2], col="blue")
-  }, bg = "grey")
+    lines3D(results()[,1], results()[,2], results()[,3], col="blue")
+  })
 }
 
-coordLabels <- c("test1", "test2")
-
 ui <- fluidPage(
-  sidebarLayout(
-    sidebarPanel(
-      viewPointUI(coordLabels),
-      viewAngleUI(coordLabels)
+  verticalLayout(
+    tabsetPanel(
+      tabPanel(
+        "parameters",
+        inputPanel(
+          p("Lorenz parameters"),
+          numericInput("sigma", "sigma", 10.0, step = 0.1),
+          numericInput("rho",   "rho",   28.0, step = 0.1),
+          numericInput("beta",  "beta",  (8.0/3.0), step = 0.1)
+        )
+      ),
+      tabPanel(
+        "initial conditions",
+        inputPanel(
+          p("Lorenz initial conditions"),
+          numericInput("x", "x", 0.1),
+          numericInput("y", "y", 0.1),
+          numericInput("z", "z", 0.1)
+        )
+      ),
+      tabPanel(
+        "view point",
+        viewPointUI(coordLabels)
+      ),
+      tabPanel(
+        "view angle",
+        viewAngleUI(coordLabels)
+      )
     ),
-    mainPanel(
-      textOutput("echo"),
-      plotOutput("distPlot"))
+    plotOutput("distPlot"),
+    htmlOutput("echo")
+    #tableOutput("results")
   )
 )
 
