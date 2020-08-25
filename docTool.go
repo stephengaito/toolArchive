304bdcfb002bad1ccb9294a0c35128c72fb65e7f
Stephen Gaito <stephen@perceptisys.co.uk>
2020-08-25 06:52:55 +0000

    refactored docTool to allow semi-automatic indexing

--- docTool.go
+++ docTool.go
@@ -21,10 +21,95 @@ import (
    "os"
    "os/exec"
    "path"
-   "path/filepath"
    "regexp"
    "strings"
 )
+
+
+var md2htmlRegexp   = regexp.MustCompile("\\.md\\)")
+var ipAddressRegexp = regexp.MustCompile("LOCAL_IP_ADDRESS")
+var dirIndexRegexp  = regexp.MustCompile("DIRECTORY_INDEX")
+var md = goldmark.New(
+    goldmark.WithExtensions(extension.GFM),
+    goldmark.WithParserOptions(
+      parser.WithAutoHeadingID(),
+    ),
+    goldmark.WithRendererOptions(
+      //html.WithHardWraps(),
+      html.WithXHTML(),
+      html.WithUnsafe(),
+    ),
+  )
+  
+func walkDir(mdDir string, htmlDir string) {
+  fmt.Printf("Working in: [%s] (%s)\n", mdDir, htmlDir)
+  os.MkdirAll(htmlDir, 0755)
+
+  mdFiles := make([]string, 0)
+  
+  files, err := ioutil.ReadDir(mdDir)
+  if err != nil { fmt.Printf("docTool ReadDir error: %v", err) }
+  
+  for _, file := range files {
+    aFile := file.Name()
+    filePath := path.Join(mdDir, aFile)
+    htmlFile := path.Join(htmlDir, aFile)
+    
+    if strings.HasSuffix(aFile, ".md") {
+      mdFiles = append(mdFiles, aFile)
+    } else if file.IsDir() {
+      walkDir(filePath, htmlFile)
+    } else {
+      fmt.Printf("  Copying [%s]\n    to [%s]\n", filePath, htmlFile)
+      cmd := exec.Command("cp", filePath, htmlFile)
+      cmd.Stdout = os.Stdout
+      cmd.Stderr = os.Stderr
+      err := cmd.Run()
+      if err != nil { fmt.Printf("docTool cp error: %v", err) }
+    }
+  }
+
+  indexList := make([]string, 0)
+  for _, aFile := range mdFiles {
+    if strings.EqualFold(aFile, "index.md") { continue }
+    fmt.Printf("indexing: [%s]\n", aFile)
+    indexList = append(indexList,
+      fmt.Sprintf("- [%s](%s)\n", strings.TrimSuffix(aFile, ".md"), aFile),
+    )
+  }
+  indexStr := strings.Join(indexList, "\n")
+  fmt.Printf("indexed: [%s]\n", indexStr)
+  
+  for _, aFile := range mdFiles {
+    filePath := path.Join(mdDir, aFile)
+    htmlFile := path.Join(htmlDir, aFile)
+    htmlFile  = strings.Replace(htmlFile, ".md", ".html", 1)
+    fmt.Printf("  Converting [%s]\n    to [%s]\n", filePath, htmlFile)
+    mdFileBytes, err := ioutil.ReadFile(filePath)
+    if err != nil {
+      fmt.Printf("docTool could not read [%s]\n", filePath)
+      continue
+    }
+    htmlFileBytes  := dirIndexRegexp.ReplaceAll(mdFileBytes, []byte(indexStr))
+    htmlFileBytes   = md2htmlRegexp.ReplaceAll(htmlFileBytes, []byte(".html)"))
+    localIpAddress := os.Getenv("ipAddress")
+    htmlFileBytes   = ipAddressRegexp.ReplaceAll(htmlFileBytes, []byte(localIpAddress))
+    var buf bytes.Buffer
+    err = md.Convert(htmlFileBytes, &buf)
+    if err != nil {
+      fmt.Printf("docTool could not convert [%s] to Markdown error: %v", filePath, err) 
+      continue
+    }
+      
+    err = ioutil.WriteFile(htmlFile, buf.Bytes(), 0644)
+    if err != nil {
+      fmt.Printf("docTool could not write [%s] error: %v", htmlFile, err) 
+      continue
+    }
+  }
+  fmt.Println("---")
+}
+
 func main() {
 
   if len(os.Args) != 3 {
@@ -40,63 +125,8 @@ func main() {
   docsPath := os.Args[1]
   htmlPath := os.Args[2]
 
-  md2htmlRegexp   := regexp.MustCompile("\\.md\\)")
-  ipAddressRegexp := regexp.MustCompile("LOCAL_IP_ADDRESS")
-
-  md := goldmark.New(
-    goldmark.WithExtensions(extension.GFM),
-    goldmark.WithParserOptions(
-      parser.WithAutoHeadingID(),
-    ),
-    goldmark.WithRendererOptions(
-      //html.WithHardWraps(),
-      html.WithXHTML(),
-      html.WithUnsafe(),
-    ),
-  )
-
   os.MkdirAll(docsPath, 0755)
   os.MkdirAll(htmlPath, 0755)
-  
-  err := filepath.Walk(docsPath, func(filePath string, info os.FileInfo, err error) error {
-    if err != nil { return err }
-    
-    htmlFile := strings.Replace(
-      filePath,
-      docsPath,
-      htmlPath,
-      1,
-    )
-    os.MkdirAll(path.Dir(htmlFile), 0755)
 
-    if strings.HasSuffix(filePath, ".md") {
-      htmlFile =  strings.Replace(htmlFile, ".md", ".html", 1)
-      fmt.Printf("converting [%s] to [%s]\n", filePath, htmlFile)
-      
-      mdFileBytes, err := ioutil.ReadFile(filePath)
-      if err != nil { return err }
-      htmlFileBytes    := md2htmlRegexp.ReplaceAll(mdFileBytes, []byte(".html)"))
-      localIpAddress := os.Getenv("ipAddress")
-      htmlFileBytes   = ipAddressRegexp.ReplaceAll(htmlFileBytes, []byte(localIpAddress))
-      
-      var buf bytes.Buffer
-      err = md.Convert(htmlFileBytes, &buf)
-      if err != nil { return err }
-      
-      err = ioutil.WriteFile(htmlFile, buf.Bytes(), 0644)
-      if err != nil { return err }
-    } else if ! info.IsDir() {
-      fmt.Printf("Copying [%s] to [%s]\n", filePath, htmlFile)
-      cmd := exec.Command("cp", filePath, htmlFile)
-      cmd.Stdout = os.Stdout
-      cmd.Stderr = os.Stderr
-      err := cmd.Run()
-      if err != nil { return err }
-    }
-    return nil
-  })
-  if err != nil {
-    fmt.Printf("docTool error: %v", err)
-  }
-  
+  walkDir(docsPath, htmlPath)
 }
